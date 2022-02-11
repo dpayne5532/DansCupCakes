564c69922711eeeba399beb62e9e3719b3dd5e78
Dan Payne <dpayne5532@gmail.com>
2022-02-10 17:26:05 -0700

    Initial Commit

---
+++ DansCupcakes/DansCupcakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DansCupcakesApp.swift
+//  DansCupcakes
+//
+//  Created by Dan Payne on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DansCupcakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
